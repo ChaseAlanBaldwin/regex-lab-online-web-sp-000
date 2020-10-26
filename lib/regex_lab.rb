3f169bcf9f8959592295248479174ba248829dd8
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-10-26 16:05:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,23 @@
 def starts_with_a_vowel?(word)
-
+  array = word.scan(/^[aieouAIEOU].*/)
+  if array.length > 0
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  array = text.scan(/^[un]|[Un].$[ing]/)
+  if array.length > 0
+    return array
+  end
 end
 
 def words_five_letters_long(text)
-
+  array = []
+  array.push(text.scan(/\b\w{1,10}\b/))
+  return array
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
